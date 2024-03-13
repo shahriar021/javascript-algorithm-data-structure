0884b4f0226cc0f0b0fb8df13415be210c258c1a
shahriar021 <shahriarc19@gmail.com>
2024-03-14 02:12:00 +0600

    bubble sort added

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+const bubble = (arr) => {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+};
+const arr = [-3, 6, 8, 2, 5, 10];
+bubble(arr)
+console.log(arr)
